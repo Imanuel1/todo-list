f24baa72adbc2c5e55d5f2352a3b62150dd47bd5
imanuel <toswim100@gmail.com>
2021-05-02 21:48:37 +0300

    using flex on all the classes to correct the filedes

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -4,8 +4,8 @@ import './Task.css'
 function Task({text, isComplete, isShared}) {
     return (
         <div className="c-task">
-            <div className="is-completed"></div>
-            <p className="h5"></p>
+            <div className="is-completed">{isComplete?"V":"X"}</div>
+            <p className="h4">{text}</p>
             <div className="is-hidden">
                 remove/ share / edit
             </div>
--- src/pages/TasksPage/TasksPage.js
+++ src/pages/TasksPage/TasksPage.js
@@ -1,46 +1,38 @@
 import React, { useState } from 'react';
-import { Container, FormControl, InputGroup, Row } from 'react-bootstrap';
+import { Container, Form, FormControl, InputGroup, Row } from 'react-bootstrap';
 import Task from '../../components/Task/Task';
 import taskModel from '../../model/TaskModel';
 import './TasksPage.css'
 
 function TasksPage() {
     const [addTask, setAddTask] = useState(null);
-    const [tasks, setTasks] = 
-    useState([new taskModel("email rachel", false,false), new taskModel("throw garbage", true,false)]);
+    const [tasks, setTasks] =
+        useState([new taskModel("email rachel", false, false), new taskModel("throw garbage", true, false)]);
 
     return (
         <Container className="p-tasks-page">
-            <h4>Website todo</h4>
+            <p className="h2">Website todo</p>
             <div className="add-task-container">
-                <input className="add-task" placeholder="add new task" value={addTask} onChange={(e) => setAddTask(e.target.value)}></input>
+                <Form>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Control type="text" placeholder="Add new task" value={addTask} onChange={(e) => setAddTask(e.target.value)}/>
+                    </Form.Group>
+                </Form>
             </div>
             <div className="tasks-container">
                 {
-                    tasks.map( task => 
-                        {
-                        <Row><Task text={task.text} isComplete={task.isComplete} isShared={false}/></Row>
-                        })
+                    tasks.map(task => {
+                        return <Row><Task text={task.task} isComplete={task.isComplete} isShared={task.isShared} /></Row>
+                    })
                 }
-                
+
             </div>
             <div className="task-footer">
-                <p>items left</p>
-                <p>All</p>
-                <p>Active</p>
-                <p>Completed</p>
+                <p className="h4">{tasks.length} items left</p>
+                <p className="h4 to-the-right">All</p>
+                <p className="h4">Active</p>
+                <p className="h4">Completed</p>
             </div>
-            {/* <InputGroup className="mb-3">
-                <InputGroup.Prepend>
-                    <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
-                </InputGroup.Prepend>
-                <FormControl
-                    placeholder="Username"
-                    aria-label="Username"
-                    aria-describedby="basic-addon1"
-                />
-            </InputGroup> */}
-
         </Container>
     );
 }
